69e786dab86b0bb95a9fbe3de60a3068ff05ef51
Chuck Junior <59852540+ceejay-el@users.noreply.github.com>
2020-09-20 04:41:28 -0700

    Update app.js

--- Shopify challenge/app.js
+++ Shopify challenge/app.js
@@ -8,11 +8,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const methodOverride = require("method-override");
 // end of requiring dependencies
+
+
 
 // varibale declarations
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoURI = "mongodb://localhost:27107/photogallerydb";
 // end of variable declarations
 
 /**
@@ -27,17 +32,36 @@ app.use(bodyParser.urlencoded({extended: true}));
  ## DATABASE 
  * connect to the database
  * create model for gallery collection
+ * create storage engine
+ * initialize gridfs stream
  */
 // connect to database
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://{}", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+const connect = mongoose.createConnection(mongoURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 // create model for gallery collection
 const gallerySchema = mongoose.Schema({
-    path: {type: String},
-    caption: {type: String}
+    path: {type: String},   // store path of uploaded image
+    caption: {type: String} // user caption
 });
 const Gallery = mongoose.model("Gallery", gallerySchema);
+
+// create storage engine
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: function(req, file){
+        return {
+            filename: "file_" + Date.now()
+        }
+    }
+});
+const upload = multer({storage});
+
+// initialize gridfs stream
+connect.once("open", function(){
+    let gfs = new mongoose.mongo.GridFSBucket(connect.db);
+});
+
+
 
 
 app.get("/", function(request, respond){
@@ -46,4 +70,4 @@ app.get("/", function(request, respond){
 
 app.listen(port, function(){
     console.log("server is listening on port " + port);
-})
+});
